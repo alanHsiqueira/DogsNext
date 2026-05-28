f9f3e81eb22b12acea32a190930adaa7f83998b5
Alan Herculino <alan.siqueira.25782@aluno.facape.br>
2026-05-28 11:11:04 -0300

    feat: add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import styles from './header.module.css';
+import Image from 'next/image';
+
+export default async function Header() {
+  const user = false;
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} href={'/'}>
+          <Image src={'/assets/dogs.svg'} width={28} height={28} priority />
+        </Link>
+        {user ? (
+          <Link className={styles.login} href={'/login'}>
+            Dogs
+          </Link>
+        ) : (
+          <Link className={styles.login} href={'/login'}>
+            Login | Criar
+          </Link>
+        )}
+      </nav>
+    </header>
+  );
+}
